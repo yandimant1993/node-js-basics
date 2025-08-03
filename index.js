af3c158030fd2ca5618b574f2b97879e9b8db3c8
yandimant1993 <yan1993dimant@gmail.com>
2025-08-03 17:29:45 +0200

    Print timeStamp

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 
+import fs from 'fs'
 
-console.log('hi')
+demoAsync()
+
+function demoAsync() {
+    fs.readFile('time-stamp.txt', 'utf8', (err, contents) => {
+        if (err) return console.log('Cannot read file')
+        const miliSeconds = contents.split('\r\n')
+        console.log(miliSeconds)
+        var seconds = miliSeconds.map((milisec) => (+milisec / 1000))
+        console.log(seconds)
+    })
+    console.log('after calling readFile:')
+}
